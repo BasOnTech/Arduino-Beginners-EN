6abaa9fe5b1dff9616b6a81ac3afeae0bce8a555
BasOnTech <info@basontech.com>
2018-03-09 15:37:03 +0100

    Added buzzer

---
+++ beeper-buzzer/README.md
@@ -0,0 +1,2 @@
+## Sound from a buzzer / beeper / piezo speaker
+Part of the Bas on Tech Arduino YouTube lessons - More info at http://BasOnTech.com
---
+++ beeper-buzzer/melody/melody.ino
@@ -0,0 +1,61 @@
+/*
+ * Bas on Tech - Arduino tutorial
+ * http://BasOnTech.com
+ * 
+ *
+ * PIN CONNECTIONS
+ *
+ * GND  --> GND
+ * 5V --> 5V
+ * Data --> 12
+ *
+ */
+
+// Import the pitches per tone
+#include "pitches.h"
+
+// set the variable "buzzerPin" to 12
+int buzzPin = 12;             
+
+// Melody notes
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note duration: 4 = quarter note, 8 = eighth note etc.
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup(void) {
+  pinMode(buzzPin, OUTPUT);         //  set the buzzerPin as output
+}
+
+void loop() {
+  playMelody();                     // call the "playMelody" function
+  delay(10000);                     // pause 10 seconds
+}
+
+void playMelody() {
+  
+  // iterate over all notes in melody[]
+  for (int i = 0; i <= 8; i++) {
+
+    // In order to calculate the correct note length we take 1 second and divide it by the type of note
+    // for example: a quarter note is 1000 / 4, an eighth note 1000 / 8 etc.
+    int noteDuration = 1000 / noteDurations[i];
+
+    // Call tone() with the current note of the melody with the calculated duration of the note.
+    tone(buzzPin, melody[i], noteDuration);
+
+    // in order to distinguish the notes, we set a minimal pause between two notes
+    // it turned out the note length plus 30% works fine.
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    // pause for the calculated duration
+    delay(pauseBetweenNotes);
+    
+    // stop playing the current note
+    noTone(buzzPin);
+  }
+}
