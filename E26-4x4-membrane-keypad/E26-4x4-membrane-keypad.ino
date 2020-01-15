a295888496fbb30e573462dfddaba88eb20d797b
BasOnTech <info@basontech.com>
2020-01-15 15:20:29 +0100

    Added E26

---
+++ E26-4x4-membrane-keypad/E26-4x4-membrane-keypad.ino
@@ -0,0 +1,48 @@
+/*
+   Bas on Tech - 4x4 Membrane keypad
+
+   This course is part of the courses on https://arduino-tutorials.net
+
+   (c) Copyright 2020 - Bas van Dijk / Bas on Tech
+   This code and course is copyrighted. It is not allowed to use these courses commercially
+   without explicit written approval
+
+   YouTube:    https://www.youtube.com/c/BasOnTech
+   Facebook:   https://www.facebook.com/BasOnTechChannel
+   Instagram:  https://www.instagram.com/BasOnTech
+   Twitter:    https://twitter.com/BasOnTech
+
+*/
+
+#include <Keypad.h>
+
+const byte ROWS = 4; // four rows
+const byte COLS = 4; // four columns
+
+// Map the buttons to an array for the Keymap instance
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte colPins[ROWS] = {5, 4, 3, 2}; // Pins used for the rows of the keypad
+byte rowPins[COLS] = {9, 8, 7, 6}; // Pins used for the columns of the keypad
+
+// Initialise the Keypad
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);   // Initialise the serial monitor
+}
+
+void loop() {
+
+  // Read the pushed button
+  char button = customKeypad.getKey();
+
+  if (button) {
+    Serial.println(button);
+  }
+}
---
+++ E26-4x4-membrane-keypad/README.md
@@ -0,0 +1,7 @@
+# 4x4 membrane keypad
+Part of the Bas on Tech Arduino YouTube tutorials - More info at https://arduino-tutorials.net
+
+Subscribe to the Bas on Tech YouTube channel via http://www.youtube.com/c/BasOnTech?sub_confirmation=1
+
+## The circuit
+![alt text](./E26-4x4-keypad-membrane-keypad.png "the circuit")
